18842f882b7d6305647128a40a4a85026804778a
Shin Asakawa <asakawa@ieee.org>
2018-09-17 06:15:19 +0900

    first commit

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='wbai_aphasia',
+      version='0.0.1',
+      description='WBAI Aphasia handoson',
+      author='Shin Asakawa',
+      author_email='asakawa@ieee.org',
+      url='https://github.com/ShinAsakawa/wbai_aphasia',
+      download_url='https://github.com/ShinAsakawa/wbai_aphasia/',
+      license='Apache 2.0',
+      })
